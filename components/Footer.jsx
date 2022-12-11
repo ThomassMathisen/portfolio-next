51fe7205d7dd947cb4f62ca8f6893a9e0d6516e2
ThomassMathisen <thomasmathisen@hotmail.com>
2022-12-11 16:46:22 +0100

    Create pages and bug fixes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import styles from "../styles/Footer.module.css";
+import Image from "next/legacy/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardL}>
+        <h1 className={styles.title}>AV0CAD0 CREATIVES.</h1>
+        <h1 className={styles.linkTitle}>
+          <Link href="/contact" className={styles.link} passHref>
+            <>
+              <span className={styles.linkText}>WORK WITH US</span>
+              <Image
+                src={process.env.NEXT_PUBLIC_URL + "/img/link.png"}
+                width="40"
+                height="40"
+                alt=""
+              />
+            </>
+          </Link>
+        </h1>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          123 5TH AVENUE,
+          <br />
+          NEW YORK, USA
+        </div>
+        <div className={styles.cardItem}>
+          CONTACT@AVOCADO.COM
+          <br /> 123 456 7890
+        </div>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          FOLLOW US:
+          <br /> __FB __IN __BE __TW
+        </div>
+        <div className={styles.cardItem}>
+          ALL RIGHTS RESERVED
+          <br />© 2022
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-
   return (
     <div className={styles.container}>
       <Link href="/">AV0CAD0</Link>
@@ -25,32 +24,32 @@ const Navbar = () => {
           <Link href="/contact">CONTACT</Link>
         </li>
       </ul>
-      <div className={styles.hambuger} onClick={() => setOpen(!open)}>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
+      <div className={styles.hamburger} onClick={() => setOpen(!open)}>
+        <div className={styles.line} />
+        <div className={styles.line} />
+        <div className={styles.line} />
       </div>
       <ul
         onClick={() => setOpen(false)}
         className={styles.menu}
-        style={{ right: open ? "0px" : "-50vew" }}
+        style={{ right: open ? "0px" : "-50vw" }}
       >
         <li className={styles.menuItem}>
           <Link href="/">HOME</Link>
         </li>
-        <li className={styles.listItem}>
+        <li className={styles.menuItem}>
           <Link href="/products/design">DESIGN</Link>
         </li>
-        <li className={styles.listItem}>
+        <li className={styles.menuItem}>
           <Link href="/products/development">DEVELOPMENT</Link>
         </li>
-        <li className={styles.listItem}>
+        <li className={styles.menuItem}>
           <Link href="/products/production">PRODUCTION</Link>
         </li>
-        <li className={styles.listItem}>
+        <li className={styles.menuItem}>
           <Link href="/products/photography">PHOTOGRAPHY</Link>
         </li>
-        <li className={styles.listItem}>
+        <li className={styles.menuItem}>
           <Link href="/contact">CONTACT</Link>
         </li>
       </ul>
